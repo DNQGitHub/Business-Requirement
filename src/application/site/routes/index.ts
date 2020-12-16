6d1b21c1bbc04c61a25c99b0155de4b0268c123d
DNQ <dnq.httt@gmail.com>
2020-12-17 01:04:38 +0700

    Update index.ts

--- src/application/site/routes/index.ts
+++ src/application/site/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import HomeController from '../controllers/HomeController';
+import SubscriberController from '../controllers/SubscriberController';
 import ErrorHandler from '../middlewares/ErrorHandler';
 
 const router = Router();
 
 router.get('/', new HomeController().index);
+
+router.get('/subscribers', new SubscriberController().index);
 
 router.use(ErrorHandler);
 
