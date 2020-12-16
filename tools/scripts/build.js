cc671403cd3ad0e4db8a3d3a5e981bac30fc3139
DNQ <dnq.httt@gmail.com>
2020-12-16 14:43:59 +0700

    Update build.js

--- tools/scripts/build.js
+++ tools/scripts/build.js
@@ -2,32 +2,27 @@ const fs = require('fs-extra');
 const childProcess = require('child_process');
 
 const copiedFiles = {
-    './src/infrastructure/ldap/certs/certificate.pem': './dist/infrastructure/ldap/certs/certificate.pem',
-    './src/infrastructure/ldap/certs/privatekey.pem': './dist/infrastructure/ldap/certs/privatekey.pem',
+	'./src/infrastructure/ldap/certs/certificate.pem': './dist/infrastructure/ldap/certs/certificate.pem',
+	'./src/infrastructure/ldap/certs/privatekey.pem': './dist/infrastructure/ldap/certs/privatekey.pem',
 };
 
 try {
-    fs.removeSync('./dist/');
-    
-    for( const srcFilename in copiedFiles ) {
-        const destFilename = copiedFiles[ srcFilename ];
-        
-        if( fs.existsSync( srcFilename ) ) {
-            const destPath = destFilename.substring( 0, destFilename.lastIndexOf('/') );
+	fs.removeSync('./dist/');
 
-            if( fs.pathExistsSync( destPath ) === false )
-                fs.mkdirSync( destPath, { recursive: true } );
+	for (const srcFilename in copiedFiles) {
+		const destFilename = copiedFiles[srcFilename];
 
-            fs.copyFileSync( srcFilename, destFilename );
-        }
-    }
+		if (fs.existsSync(srcFilename)) {
+			const destPath = destFilename.substring(0, destFilename.lastIndexOf('/'));
 
-    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
-    proc.on('close', (code) => {
-        if (code !== 0) {
-            throw Error("Build failed")
-        }
-    })
+			if (fs.pathExistsSync(destPath) === false) fs.mkdirSync(destPath, { recursive: true });
+
+			fs.copyFileSync(srcFilename, destFilename);
+		}
+	}
+
+	childProcess.execSync('tsc --build tsconfig.prod.json');
 } catch (err) {
-    console.log(err);
+	// eslint-disable-next-line no-console
+	console.log(err);
 }
