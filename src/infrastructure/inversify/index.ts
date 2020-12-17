3b50e81c00c591fb36c4a75de0f8821f7526d2ea
DNQ <dnq.httt@gmail.com>
2020-12-18 00:09:50 +0700

    Update index.ts

--- src/infrastructure/inversify/index.ts
+++ src/infrastructure/inversify/index.ts
@@ -2,6 +2,8 @@ import { Container } from 'inversify';
 import 'reflect-metadata';
 import ErrorCollector from '@infrastructure/utilities/ErrorCollector';
 import Nodemailer from '@infrastructure/nodemailer';
+import ISubscriberDao from '@domain/daos/ISubscriberDao';
+import SubscriberDao from '@infrastructure/objection-js/daos/SubscriberDao';
 
 const container = new Container({
 	autoBindInjectable: true,
@@ -13,5 +15,8 @@ container.bind<ErrorCollector>('ErrorCollector').to(ErrorCollector);
 
 // Mail
 container.bind<Nodemailer>('Nodemailer').to(Nodemailer);
+
+// Daos
+container.bind<ISubscriberDao>('ISubscriberDao').to(SubscriberDao);
 
 export default container;
