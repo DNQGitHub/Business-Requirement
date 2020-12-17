1eb4685d749660c2bdee798548b4032f91f4eb32
Quang Doan Nhat <quang.doannhat@gameloft.com>
2020-12-17 17:49:30 +0700

    Update IDao.ts

--- src/domain/daos/IDao.ts
+++ src/domain/daos/IDao.ts
@@ -4,7 +4,7 @@ export default interface IDao<T extends IEntity> {
 	insert(entity: T): Promise<T | null>;
 	findOneById(id: string): Promise<T | null>;
 	all(): Promise<Array<T>>;
-	count(): Promise<number>;
+	countAll(): Promise<number>;
 	delete(entity: T): Promise<T | null>;
 	update(entity: T): Promise<T | null>;
 }
