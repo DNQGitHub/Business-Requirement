32aca0916f8092403594dadcd1f600aded9a3116
DNQ <dnq.httt@gmail.com>
2020-12-18 00:10:22 +0700

    Update SubscriberController.ts

--- src/application/api/controllers/SubscriberController.ts
+++ src/application/api/controllers/SubscriberController.ts
@@ -1,11 +1,20 @@
+import CreateSubscriberCommand from '@presentation/requests/api/CreateSubscriberCommand';
+import IoC from '@infrastructure/inversify';
 import { NextFunction, Request, Response } from 'express';
+import CreateSubscriberCommandHandler from '../command-handlers/CreateSubscriberCommandHandler';
 
 export default class SubscriberController {
-	createSubscriber(req: Request, res: Response, next: NextFunction) {
-		res.json({});
+	async createSubscriber(req: Request, res: Response, next: NextFunction) {
+		const command = new CreateSubscriberCommand(req);
+		const commandHandler = IoC.get(CreateSubscriberCommandHandler);
+		await commandHandler.handle(command);
+
+		res.json({
+			success: true,
+		});
 	}
 
-	listAllSubscribers(req: Request, res: Response, next: NextFunction) {
+	async listAllSubscribers(req: Request, res: Response, next: NextFunction) {
 		res.json({});
 	}
 }
