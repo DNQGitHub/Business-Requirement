d5a8bddfa7cc4b01085cc17761ff6b6a2a72bd6b
DNQ <dnq.httt@gmail.com>
2020-12-17 23:40:31 +0700

    Create SubscriberDao.ts

---
+++ src/infrastructure/objection-js/daos/SubscriberDao.ts
@@ -0,0 +1,55 @@
+import ISubscriberDao from '@domain/daos/ISubscriberDao';
+import SubscriberEntity from '@domain/entities/Subscriber';
+import { PartialModelObject, QueryBuilder } from 'objection';
+import SubscriberModel from '../models/Subscriber';
+import Dao from './Dao';
+
+export default class SubscriberDao extends Dao<SubscriberEntity, SubscriberModel> implements ISubscriberDao {
+	initQuery(): QueryBuilder<SubscriberModel, SubscriberModel[]> {
+		return SubscriberModel.query();
+	}
+
+	convertModelToEntity(model: SubscriberModel) {
+		if (model === null || model === undefined) return null;
+
+		const dbJson = model.$toDatabaseJson();
+
+		const entity = SubscriberEntity.create(
+			{
+				name: dbJson['name'],
+				email: dbJson['email'],
+				phoneNumber: dbJson['phone_number'],
+				imageUrl: dbJson['image_url'],
+				createdDate: dbJson['created_at'],
+				updatedDate: dbJson['updated_at'],
+			},
+			dbJson['id']
+		);
+
+		return entity;
+	}
+
+	convertEntityToPartialModelObject(entity: SubscriberEntity): PartialModelObject<SubscriberModel> {
+		const model = new SubscriberModel();
+
+		model.$set({
+			id: entity.id,
+			name: entity.name,
+			email: entity.email,
+			phone_number: entity.phoneNumber,
+			image_url: entity.imageUrl,
+			created_at: entity.createdDate,
+			updated_at: entity.updatedDate,
+		});
+
+		return model;
+	}
+
+	async findOneByEmail(email: string): Promise<SubscriberEntity | null> {
+		const query = this.initQuery();
+
+		const model = await query.findOne('email', '=', email);
+
+		return this.convertModelToEntity(model);
+	}
+}
