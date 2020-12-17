683c51e96f260e4466d8a5ef57e6f56b29ead39b
DNQ <dnq.httt@gmail.com>
2020-12-18 02:23:26 +0700

    Update routes.ts

--- src/application/routes.ts
+++ src/application/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import siteRoutes from './site/routes';
 import apiRoutes from './api/routes';
+import docsRoutes from './docs/routes';
 
 const router = Router();
 
 router.use(siteRoutes);
 router.use('/api', apiRoutes);
+router.use('/swagger-docs', docsRoutes);
 
 export default router;
