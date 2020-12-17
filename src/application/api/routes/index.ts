8f5928c0f74a39a165ee27ecb0f4cd6d50ecb4f8
DNQ <dnq.httt@gmail.com>
2020-12-17 23:54:26 +0700

    Update index.ts

--- src/application/api/routes/index.ts
+++ src/application/api/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import SubscriberController from '../controllers/SubscriberController';
-
 import ErrorHandler from '../middlewares/ErrorHandler';
+import Upload from '@infrastructure/multer/upload';
 
 const router = Router();
 
-router.post('/subscribers', new SubscriberController().createSubscriber);
+router.post('/subscribers', Upload.single('image'), new SubscriberController().createSubscriber);
 
 router.get('/subscribers', new SubscriberController().listAllSubscribers);
 
