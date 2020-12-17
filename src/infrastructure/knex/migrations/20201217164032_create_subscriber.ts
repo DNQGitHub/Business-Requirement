b77e528f7a7d2dbf17053b7c69534a2a4db487ec
Quang Doan Nhat <quang.doannhat@gameloft.com>
2020-12-17 16:54:56 +0700

    Create 20201217164032_create_subscriber.ts

---
+++ src/infrastructure/knex/migrations/20201217164032_create_subscriber.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+	return knex.schema.createTable('subscriber', table => {
+		table.increments('id').primary();
+		table.string('name');
+		table.string('email');
+		table.string('phone_number');
+		table.string('image_uri');
+	});
+}
+
+export async function down(knex: Knex): Promise<any> {}
