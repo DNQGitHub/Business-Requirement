281225e9e86ab7f77f43945556b54458f89c3ad8
DNQ <dnq.httt@gmail.com>
2020-12-18 01:06:36 +0700

    Update 20201217164032_create_subscriber.ts

--- src/infrastructure/knex/migrations/20201217164032_create_subscriber.ts
+++ src/infrastructure/knex/migrations/20201217164032_create_subscriber.ts
@@ -7,6 +7,8 @@ export async function up(knex: Knex): Promise<any> {
 		table.string('email');
 		table.string('phone_number');
 		table.string('image_url');
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(knex.fn.now());
 	});
 }
 
