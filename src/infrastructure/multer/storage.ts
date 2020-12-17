3c7d4b7cf2e23be322beac256dc5c23f9221d11d
DNQ <dnq.httt@gmail.com>
2020-12-18 00:31:42 +0700

    Update storage.ts

--- src/infrastructure/multer/storage.ts
+++ src/infrastructure/multer/storage.ts
@@ -1,10 +1,15 @@
 import Multer from 'multer';
+import fs from 'fs-extra';
 
 export default Multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'public/uploads');
+		const uploadDir = 'public/uploads';
+		if (!fs.pathExistsSync(uploadDir)) fs.mkdirSync(uploadDir);
+		cb(null, uploadDir);
 	},
 	filename: function (req, file, cb) {
-		cb(null, file.filename + '-' + Date.now());
+		console.log(file);
+		const fileName = Date.now() + '-' + file.originalname;
+		cb(null, fileName);
 	},
 });
