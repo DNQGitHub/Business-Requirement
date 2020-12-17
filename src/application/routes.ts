3a55ec2a837cc3e0986892ef402eb2fa998740b9
Quang Doan Nhat <quang.doannhat@gameloft.com>
2020-12-17 17:55:46 +0700

    Update routes.ts

--- src/application/routes.ts
+++ src/application/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import siteRoutes from './site/routes';
+import apiRoutes from './api/routes';
 
 const router = Router();
 
 router.use(siteRoutes);
+router.use(apiRoutes);
 
 export default router;
