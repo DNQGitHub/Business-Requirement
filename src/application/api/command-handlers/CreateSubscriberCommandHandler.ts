bb31f26d3bdb4763b9f49596d446d70067ba8f00
DNQ <dnq.httt@gmail.com>
2020-12-18 00:10:18 +0700

    Create CreateSubscriberCommandHandler.ts

---
+++ src/application/api/command-handlers/CreateSubscriberCommandHandler.ts
@@ -0,0 +1,27 @@
+import CommandHandler from '@infrastructure/core/abstracts/CommandHandler';
+import ValidationError from '@infrastructure/core/errors/ValidationError';
+import CreateSubscriberCommand from '@presentation/requests/api/CreateSubscriberCommand';
+
+export default class CreateSubscriberCommandHandler extends CommandHandler<CreateSubscriberCommand> {
+	async validate(command: CreateSubscriberCommand) {
+		this._ec.collect('name', () => {
+			if (!!command.name === false) throw Error('name is emtpy');
+		});
+
+		this._ec.collect('email', () => {
+			if (!!command.email === false) throw Error('email is emtpy');
+			if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gim.test(command.email) === false)
+				throw Error('email is invalid');
+		});
+
+		this._ec.collect('image', () => {
+			if (command.image.mimetype.indexOf('image') == -1) throw new Error('image is invalid');
+		});
+
+		if (this._ec.hasError()) throw new ValidationError(this._ec.errors);
+	}
+
+	public async handle(command: CreateSubscriberCommand): Promise<any> {
+		await this.validate(command);
+	}
+}
