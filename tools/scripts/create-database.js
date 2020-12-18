d6d6e6c7417c92f9f7c21875db7236331aa0487b
Quang Doan Nhat <quang.doannhat@gameloft.com>
2020-12-18 13:47:57 +0700

    Create create-database.js

---
+++ tools/scripts/create-database.js
@@ -0,0 +1,27 @@
+require('../../dist/infrastructure/core');
+const Knex = require('knex');
+const { exit } = require('process');
+
+console.log('Start Create Database');
+
+const knex = Knex({
+	client: process.env.KNEX_CLIENT,
+	connection: {
+		host: process.env.KNEX_DB_HOST,
+		port: process.env.KNEX_DB_PORT,
+		user: process.env.KNEX_DB_USERNAME,
+		password: process.env.KNEX_DB_PASSWORD,
+		charset: 'utf8mb4_unicode_ci',
+	},
+});
+
+const query = `\
+    CREATE DATABASE IF NOT EXISTS ${process.env.KNEX_DB_NAME} CHAR SET 'utf8mb4'
+`;
+
+knex.raw(query).then(result => {
+	console.log(result);
+	return true;
+});
+
+exit();
